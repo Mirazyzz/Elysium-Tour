285ec90137f83a2bea0b5f1db3ee2c4c9e90d070
Miraziz Khidoyatov <49793609+Mirazyzz@users.noreply.github.com>
2021-08-04 08:02:04 +0200

    change project ui structure

---
+++ src/pages/Home.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import Hero from '../components/Hero';
+import Banner from '../components/Banner';
+import FeaturedRooms from '../components/FeaturedRooms';
+import { Link } from 'react-router-dom';
+import Services from '../components/Services';
+
+export default function Home() {
+  return (
+    <>
+      <Hero>
+        <Banner title="Elysium Tours" subTitle="Туры в Египет от 300$">
+          <Link to="/rooms" className="btn-primary">
+            Туры
+          </Link>
+        </Banner>
+      </Hero>
+      <Services />
+      <FeaturedRooms />
+    </>
+  );
+}
